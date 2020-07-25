f021116df616e97aceb2b869c64f95ee0dada494
t-shimpo <zanjibaru7@gmail.com>
2020-07-26 06:33:18 +0900

    Userモデルにnickname属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_07_24_022712) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
