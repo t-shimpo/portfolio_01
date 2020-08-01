aca351a241663317e8f846b5bd0f7719a28de637
t-shimpo <zanjibaru7@gmail.com>
2020-08-01 14:23:54 +0900

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: :show
+  before_action :set_user, only: :show
 
   def index
 
   end
 
   def show
-    
+
   end
+
+  private
+
+  # def user_params
+  #   params.require(:user).permit(:nickname, :email, :password)
+  # end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,10 +6,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
-
-require_relative "support/controller_macros" 
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
