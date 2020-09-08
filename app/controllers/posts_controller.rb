4f0d5894bba8f00bfe56979058c9224ab6ade3fe
t-shimpo <zanjibaru7@gmail.com>
2020-09-08 09:40:22 +0900

    posts#show,edit,update,destroyの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
+  before_action :set_post, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.page(params[:page]).per(16)
@@ -20,16 +22,24 @@ class PostsController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
   end
 
   def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = "投稿は更新されました。"
+      redirect_to @post
+    else
+      render 'posts/edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    flash[:notice] = "投稿は削除されました。"
+    redirect_to @post.user
   end
 
 
@@ -47,6 +57,15 @@ class PostsController < ApplicationController
        :purchase_date,
        :post_content
      ).merge(user_id: current_user.id)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to posts_url if @post.nil?
     end
 
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -8,4 +8,27 @@ FactoryBot.define do
     hours { "not_select" }
   end
 
+  factory :hibana, class: Post do
+    title { "火花" }
+    author { "又吉直樹" }
+    publisher { "文春文庫" }
+    genre { "novel" }
+    rating { "int4" }
+    hours { "to30" }
+    purchase_date { "2020-7-15" }
+    post_content { "面白い作品でした。" }
+  end
+
+  factory :jibun_nonakani_doku, class: Post do
+    title { "自分の中に毒を持て" }
+    author { "岡本太郎" }
+    publisher { "青春文庫" }
+    genre { "business" }
+    rating { "int4_5" }
+    hours { "to40" }
+    purchase_date { "2019-12-20" }
+    post_content { "とてもためになりました。" }
+  end
+
+
 end
