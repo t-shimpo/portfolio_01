0666cf2b439ebb406acdfa9231d74ce25178ec64
t-shimpo <zanjibaru7@gmail.com>
2020-09-20 08:08:55 +0900

    model specの改善

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post) }
+
+  it 'user_idとpost_idがあれば有効であること' do
+    like = Like.new(post_id: post.id, user_id: user.id)
+    expect(like).to be_valid
+  end
+
+  it 'post_idがnilであれば無効であること' do
+    like = Like.new(post_id: nil, user_id: user.id)
+    expect(like).to_not be_valid
+  end
+
+  it 'user_idがnilであれば無効であること' do
+    like = Like.new(post_id: post.id, user_id: nil)
+    expect(like).to_not be_valid
+  end
+
+end
