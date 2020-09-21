cfd7abf6c361534fd2d4eace0c70ffb5986cd798
t-shimpo <zanjibaru7@gmail.com>
2020-09-21 22:42:08 +0900

    シードデータの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all.page(params[:page]).per(16)
+    @posts = Post.all.page(params[:page]).per(16).order('updated_at DESC')
   end
 
   def create
