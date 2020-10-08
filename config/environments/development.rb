68a2e3754d79f1dbe98c9159d5a4aa062c292dec
t-shimpo <zanjibaru7@gmail.com>
2020-10-08 19:35:12 +0900

    AWSの設定の修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     port: 587,
     user_name: Rails.application.credentials.gmail[:email],
     password: Rails.application.credentials.gmail[:password],
-    authentication: :plain,
+    authentication: :login,
     enable_starttls_auto: true
   }
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,25 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 # 画像名に日本語が使えるようにする
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'portfolio1-image'
+    config.asset_host = 'https://s3.amazonaws.com/portfolio1-image'
+    config.fog_public = false
+
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
-
-    config.fog_directory  = 'portfolio1-image'
-    config.cache_storage = :fog
   end
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
